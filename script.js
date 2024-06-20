84b936d0e29cb968e132d9c322f5f1a688c266d9
k4rth1k315ht <163455822+k4rth1k315ht@users.noreply.github.com>
2024-06-20 23:52:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 //your JS code here. If required.
+const targetLevel = document.getElementById("level");
+
+function getDonLevel(element){
+
+let level;
+
+// parentElement;
+
+// clement = element.parentElement;
+
+// element element.parentElement;
+
+while(element){
+
+level++;
+
+element element.parentElement;
+
+}
+
+return level;
+
+}
+
+const domLevel getDomLevel(targetLevel);
+
+alert("The Level of the element is: ${domLevel}');
